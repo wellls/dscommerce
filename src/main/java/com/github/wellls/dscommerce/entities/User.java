1fb8a13a731eea05e7b78eef853395dd71b7b4ad
Wellington Lima <wellington.map@hotmail.com>
2024-10-15 09:52:58 -0300

    feat: create OrderStatus enum, Order Entity and ManyToOne relationship with User Entity

--- src/main/java/com/github/wellls/dscommerce/entities/User.java
+++ src/main/java/com/github/wellls/dscommerce/entities/User.java
@@ -1,15 +1,26 @@
 package com.github.wellls.dscommerce.entities;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -21,6 +32,14 @@ public class User {
         this.phone = phone;
         this.birthDate = birthDate;
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -63,12 +82,8 @@ public class User {
         this.password = password;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
