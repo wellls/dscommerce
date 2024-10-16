88b2300df88f2c729ff4a0390280e3a8447e7b7e
Wellington Lima <wellington.map@hotmail.com>
2024-10-16 10:05:57 -0300

    feat: create OrderItem entity, OrderItemPK embeddable class to handle with Order and Product ManyToMany relationship

--- src/main/java/com/github/wellls/dscommerce/entities/Order.java
+++ src/main/java/com/github/wellls/dscommerce/entities/Order.java
@@ -3,7 +3,10 @@ package com.github.wellls.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -23,6 +26,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
@@ -64,6 +70,14 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 
     @Override
